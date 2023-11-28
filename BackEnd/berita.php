eea4877426ce6c0667d0b51c7a35ad92c23fc212
Erzamaulana <149370408+Erzamaulana@users.noreply.github.com>
2023-11-28 17:42:14 +0700

    file database
    untuk menyimpan data data pada page di website yang memerlukan database

---
+++ BackEnd/berita.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Berita (isi judul)</title>
+
+    <!-- Fav icons -->
+    <script src="https://kit.fontawesome.com/46b7ceee20.js" crossorigin="anonymous"></script>
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Caption:wght@400;700&family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+    <!-- CSS --> 
+    <link href="css/berita.css" rel="stylesheet" />
+    <link href="css/nav-end.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Navbar / Header -->
+    <div class="navcontainer" id="navcontainer">
+      <div class="nav">
+          <input type="checkbox" id="nav-check">
+          <div class="nav-header">
+              <img src="img\logo-pilpreskini.png" alt="logo-portal" width="75%">
+          </div>
+          <div class="nav-btn">
+            <label for="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div> 
+          <div class="nav-links">
+              <a href="index.php" target="_self">Home</a>
+              <a class="navactive" href="isu.php" target="_self">Isu</a>
+              <a href="pilpres.php" target="_self">Pilpres</a>
+              <a href="about.html" target="_self">About</a>
+              <img src="img\logo-km.png" alt="logo-km" width="12%" style="margin-right: 4px;">
+              <img src="img\logo-revou.png" alt="logo-revou" width="8%">             
+          </div>
+      </div>
+  </div>
+
+    <!--isi berita-->
+    <div class="grid-container" id="page">
+      <div class="site-content">
+        <div class="news">
+          <header>
+            <div class="container">
+              <nav>
+                <ul>
+                  <li><a href="#">Isu ></a></li> 
+                  <li><a href="#">Seputar Annies - Amin</a></li>
+                </ul>
+              </nav>
+            </div>
+          </header>
+          <main>
+            <div class="container">
+              <article class="article">
+                <h1 class="article-title">Pasangan AMIN Dapat Nomor Urut Satu.</h1>
+                <p class="article-date">Diperbarui 15 Nov 2023, 13:30 WIB</p>
+                <div class="article-image">
+                  <img src="https://pkb.id/files/posts/1/2023/2023-11-15/24756dba05ee8a687575a44f6c85c879_1.jpeg">
+                  <p class="caption">Anies Baswedan dan Muhaimin Iskandar mendapat nomor urut 1 (satu)(Liputan6.com/Faizal Fanani)</p>
+                </div>
+                <div class="article-content">
+                  <p>
+                    PilpresKini, Jakarta - Calon Presiden (Capres) Anies Baswedan mengaku bersyukur usai mendapatkan nomor urut. Pasangan Anies-Muhaimin mendapatkan nomor urut satu pada Pilpres 2024.
+                    "Kita semua bersyukur, satu bersyukur, dua bersyukur, tiga bersyukur. Alhamdulillah, semua pasti ada hikmahnya," kata Anies di KPU RI, Jakarta, Selasa 14 November 2023.
+                    Menurut dia, dengan telah mendapatkan nomor urut satu ini telah menjadi referensi untuk para pasangan capres-cawapres lainnya.
+                    "Dan nomor satu ini, seperti tadi. Ketika ngasih sambutan langsung jadi referensi yang nomor dua, referensi nomor tiga. Referensinya siapa? Yang nomor pertama. Jadi selalu ada hikmahnya," ujarnya.
+                    Sementara itu, Muhaimin Iskandar alias Cak Imin mengaku, nomor urut satu yang didapatkannya ini sudah sesuai dengan yang diharapkannya.
+                    "Alhamdulillah sesuai harapan dan doa semua, akhirnya pasangan Amin dapat nomor 1, nomor 1 tanda-tanda untuk menang juara 1, mohon doa restunya, moga-moga Pemilu ini lancar," ujar Gus Imin.
+                    Ketua Umum Partai Kebangkitan Bangsa (PKB) ini pun mengajak masyarakat untuk dapat ikut mengawal Pemilu 2024.
+                  </p>
+                  <p>
+                    "Kalau ingin perubahan ayo kuatkan nomor 1, kalau kita ingin Pemilu jujur, adil, bebas mari kita kawal Pemilu dengann semangat untuk mengawasi, tidak boleh ada wasit merangkap pemain, tidak boleh ada kecurangan, bukan kepentingan Amin," ujar Cak Imin.
+                    "Tapi Pemilu yang sehat, yang kuat akan menyelematkan dan memajukan demokrasi. Kalau demokrasi maju, pasti rakyat akan semakin makmur," sambung dia.
+                    Selain itu, Kapten Timnas Pemenangan Anies-Muhaimin, M Syaugi Alaydrus ingin agar dengan mendapatkan nomor urut satu bisa menang dalam satu putaran.
+                    "Terdaftar nomor satu, ambil urutan nomor satu, dapat nomor satu insyaallah satu putaran, doa ini namanya doa kan," ujar Syaugi.
+                    Menurut dia, nomor urut satu sudah sesuai dengan harapan yang mereka inginkan. Apalagi, salah satu partai Koalisi Perubahan yakni PKB mendapatkan nomor satu.
+                    "Ya pasti adalah harapannya, PKB salah satu partai kita pendukung nomor satu juga, dan mudah-mudahan nomor satu ini jadi pertanda baik. Mudah-mudahan pasangan Anies-Muhaimin ini satu kali gol," ucapnya.
+                    "Ya sesuai (harapan) dong sesuai kan kita ingin yang pertama dong. Daftar saja pertama, urutan nomor pertama dapet juga insyaallah menang pertama," pungkasnya.
+                 </p>
+                </div>
+              </article>
+            </div>
+          </main>
+        </div>
+        <div class="widget">
+            <aside id="widget-title">
+              <h4>Berita Terbaru Lainnya</h4>
+            </aside>
+            <aside id="widget-content">
+              <a href="#" class="cardcover">
+                  <div class="card"></div>
+                  <div class="card-content"><p>Jokowi Ingatkan Netralitas ASN Jelang Pemilu 2024.</p></div>
+              </a>
+              <a href="#" class="cardcover">
+                <div class="card"></div>
+                <div class="card-content"><p>Jokowi Ingatkan Netralitas ASN Jelang Pemilu 2024.</p></div>
+              </a>
+              <a href="#" class="cardcover">
+                <div class="card"></div>
+                <div class="card-content"><p>Jokowi Ingatkan Netralitas ASN Jelang Pemilu 2024.</p></div>
+              </a>
+              <a href="#" class="cardcover">
+                <div class="card"></div>
+                <div class="card-content"><p>Jokowi Ingatkan Netralitas ASN Jelang Pemilu 2024.</p></div>
+              </a>
+              <a href="#" class="cardcover">
+                <div class="card"></div>
+                <div class="card-content"><p>Jokowi Ingatkan Netralitas ASN Jelang Pemilu 2024.</p></div>
+              </a>
+              <a href="#" class="cardcover">
+                <div class="card"></div>
+                <div class="card-content"><p>Jokowi Ingatkan Netralitas ASN Jelang Pemilu 2024.</p></div>
+              </a>
+            </aside>
+        </div>
+      </div>
+    </div>
+ 
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="footercontainer">
+        <div class="footer-content">
+          <img src="img\logo-pilpreskini.png" alt="logo-portal" width="120">
+          <p style="text-align: start">PilpresKini is a news portal will provide up-to-date coverage of the
+            indonesian  presidential  election, as well as in-depth analysis for each candidate</p>
+        </div>
+        <div class="footer-bottom">
+            <div class="footer-social">
+              <ul class="social-icons">
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+            <p style="text-align: end">copyright © 2023 PilpresKini</p>
+        </div>
+      </div>
+    </footer>
+</body>
+</html>
+
+<script type="text/javascript" src="js/scriptnav.js"></script>
