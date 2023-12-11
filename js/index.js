0a75e79d9bae35ab9014829b4ce05b2ffd27a796
TheodorDani <63799480+TheodorDani@users.noreply.github.com>
2023-12-11 12:57:21 +0700

    Update index.js utk pool

--- js/index.js
+++ js/index.js
@@ -26,3 +26,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+document.getElementById('submit').addEventListener('click', function(event) {
+  event.preventDefault(); // Menghentikan perilaku bawaan tombol submit
+
+  var selectedOption = document.getElementById('candidate-input').value; // Ambil nilai pilihan
+
+  // Kirim nilai pilihan ke server (Anda perlu mengirim data ini ke server menggunakan AJAX atau fetch)
+
+  alert('Terima kasih atas partisipasi Anda dalam polling!');
+});
+
+document.getElementById('results-button').addEventListener('click', function() {
+  // Tampilkan hasil polling dari server
+  var infoElement = document.getElementById('info');
+  if (infoElement.style.maxHeight === '0px') {
+      infoElement.style.maxHeight = infoElement.scrollHeight + 'px';
+  } else {
+      infoElement.style.maxHeight = '0px';
+  }
+});
